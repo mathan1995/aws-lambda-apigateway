f3856624f6695aae5d2cc809bc20cbd25504fcc9
mathan7 <mathanganjeyakantharajah@adventus.io>
2021-12-24 21:48:00 +0530

    refactor lambda code

--- src/delete.js
+++ src/delete.js
@@ -4,32 +4,26 @@ const s3 = new AWS.S3();
 const BUCKET_NAME = process.env.FILE_UPLOAD_BUCKET_NAME;
 
 module.exports.handler = async (event) => {
-  console.log(event);
+	console.log(event);
 
-  const response = {
-    isBase64Encoded: false,
-    statusCode: 200,
-  };
+	const response = {
+		isBase64Encoded: false,
+		statusCode: 200,
+	};
 
-  try {
-    const params = {
-      Bucket: BUCKET_NAME,
-      Key: decodeURIComponent(event.pathParameters.fileKey),
-    };
-    const deleteResult = await s3.deleteObject(params).promise();
+	try {
+		const params = {
+			Bucket: BUCKET_NAME,
+			Key: decodeURIComponent(event.pathParameters.fileKey),
+		};
+		const deleteResult = await s3.deleteObject(params).promise();
 
-    response.body = JSON.stringify({
-      message: "Successfully deleted file from S3.",
-      deleteResult,
-    });
-  } catch (e) {
-    console.error(e);
-    response.body = JSON.stringify({
-      message: "Failed to delete file.",
-      errorMessage: e,
-    });
-    response.statusCode = 500;
-  }
+		response.body = JSON.stringify({ message: "Successfully deleted file from S3.", deleteResult });
+	} catch (e) {
+		console.error(e);
+		response.body = JSON.stringify({ message: "Failed to delete file.", errorMessage: e });
+		response.statusCode = 500;
+	}
 
-  return response;
+	return response;
 };
--- src/get.js
+++ src/get.js
@@ -4,32 +4,26 @@ const s3 = new AWS.S3();
 const BUCKET_NAME = process.env.FILE_UPLOAD_BUCKET_NAME;
 
 module.exports.handler = async (event) => {
-  console.log(event);
+	console.log(event);
 
-  const response = {
-    isBase64Encoded: false,
-    statusCode: 200,
-  };
+	const response = {
+		isBase64Encoded: false,
+		statusCode: 200,
+	};
 
-  try {
-    const params = {
-      Bucket: BUCKET_NAME,
-      Key: decodeURIComponent(event.pathParameters.fileKey),
-    };
-    const data = await s3.getObject(params).promise();
+	try {
+		const params = {
+			Bucket: BUCKET_NAME,
+			Key: decodeURIComponent(event.pathParameters.fileKey),
+		};
+		const data = await s3.getObject(params).promise();
 
-    response.body = JSON.stringify({
-      message: "Successfully retrieved file from S3.",
-      data,
-    });
-  } catch (e) {
-    console.error(e);
-    response.body = JSON.stringify({
-      message: "Failed to get file.",
-      errorMessage: e,
-    });
-    response.statusCode = 500;
-  }
+		response.body = JSON.stringify({ message: "Successfully retrieved file from S3.", data });
+	} catch (e) {
+		console.error(e);
+		response.body = JSON.stringify({ message: "Failed to get file.", errorMessage: e });
+		response.statusCode = 500;
+	}
 
-  return response;
+	return response;
 };
